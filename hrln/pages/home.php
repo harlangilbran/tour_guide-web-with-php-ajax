a809fb81c9d09c586c7c05f6b27cecfa8891219e
Harlan Gilbran <167880900+harlangilbran@users.noreply.github.com>
2024-12-10 12:05:15 +0700

    Update home.php

--- hrln/pages/home.php
+++ hrln/pages/home.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pasaman Barat Tour Guide</title>
+    <title>Tour Guide</title>
     <!-- Link to offline Bootstrap CSS -->
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <!-- Custom CSS -->
